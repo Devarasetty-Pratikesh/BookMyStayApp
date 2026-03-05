08650c59dbe9837e53160080e2106425a05342e9
Pratikesh <pratikeshdevarasetty1315@gmail.com>
2026-03-05 09:13:32 +0530

    Prethikesh Added feature/UC1-WelcomePage

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,20 @@
+/**
+ * BookMyStayApp
+ * Entry point for the Hotel Booking Application
+ *
+ * @author Prethikesh
+ * @version 1.0
+ */
+
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println("     Welcome to Book My Stay App    ");
+        System.out.println("        Hotel Booking System        ");
+        System.out.println("            Version 1.0             ");
+        System.out.println("====================================");
+
+    }
+}
